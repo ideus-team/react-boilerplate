d06c8a7dc0000173f263f8eb6d4bcdabaa9519bb
Yulia <yuliya.chudak@ideus.biz>
2017-11-14 19:30:52 +0300

    Set base structure

---
+++ src/containers/LoginContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from '../components/Login';
+
+/**
+ * in case using state must be overwritten into a react class
+ * */
+function LoginContainer() {
+  return (
+    <Login />
+  );
+}
+
+export default LoginContainer;
